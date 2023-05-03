19e5971e91b8e337cd7b1e18713493e820f1b37e
aktiangco <113158472+aktiangco@users.noreply.github.com>
2023-05-02 19:52:13 -0700

    need to learn more in recursion

--- solution5.py
+++ solution5.py
@@ -1,1 +1,18 @@
 # Write code for algorithm 5 below
+# * Write a function that checks whether a string is a palindrome or not. The program should take in a string and return True if the string is a palindrome and False if not
+
+def isPalindrome(word):
+    
+    # base case
+    if len(word) < 2:
+        return True 
+    
+    else: 
+        if word[0] == word[-1]:
+            return isPalindrome(word[1:-1])
+        else: 
+            return False
+
+
+sample_word = 'radar'
+print(isPalindrome(sample_word))
